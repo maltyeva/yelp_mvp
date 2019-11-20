6e1e5cd483c88b4a5111f7a1e0f83d26f9879dfc
Maria <maria.altyeva@gmail.com>
2019-11-20 09:24:31 +0800

    added restaurant scaffold and seeds

--- Gemfile
+++ Gemfile
@@ -21,13 +21,15 @@ gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
-# Use Redis adapter to run Action Cable in production
+# gem 'jbuilder', '~> 2.5'
+# Use Redis adaprailster to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'simple_form'
+
+gem 'faker'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+
+20.times do
+  r = Restaurant.create!(name: Faker::Hipster.word,
+                         address: Faker::Address.street_address,
+                         description: Faker::Dessert.variety,
+                         stars: rand(1..5))
+  puts "Created #{r.name}"
+end
